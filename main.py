4ed8493b26615617879f905ec8f7d2e3fcec8947
João Eurico Bokomoko Lima <bokomoko@gmail.com>
2020-09-11 16:51:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+def radixSort( vector ):
+  winput = list(vector)
+  # calculate de number of passes = number of digits of max
+  passes = math.ceil(math.log10(max(vector)))
+  woutput =[]
+  wdivider = 1 
+  for i in range(passes) :
+    # empty output
+    woutput = [0]*len(winput)
+    # restart counters
+    numbers = [0]*10
+    # counts how many times each digit occurred
+    for elem in winput  :
+      digit = (elem // wdivider) % 10 
+      numbers[int(digit)] +=1
+    #prefix sum !! This is the trick !
+    for w in range(1,10):
+      numbers[w] += numbers[w-1]
+    
+    #now moving from input to outuput
+    for elem in winput[::-1]:
+      digit = (elem // wdivider) % 10
+      woutput[numbers[digit]-1] = elem 
+      numbers[digit] -= 1  # one less elem with that digit
+    
+    winput = list(woutput)  # outuput 
+    wdivider *= 10
+
+  return woutput
+    
+
+# main program begins here
+import random 
+import math
+
+# read unsorted data from file
+myList = list(map(int,open("unsorted.dat", "r").read().split(",")))
+
+coisa = radixSor(myList)
+print(coisa)
